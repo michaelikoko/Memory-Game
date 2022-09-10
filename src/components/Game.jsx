401e7cb6e393b3ce90b1326f1b57955d1c087230
michael_ikoko <michaelikoko.o@gmail.com>
2022-09-10 15:26:26 +0100

    Changed image location

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -6,7 +6,7 @@ import Footer from "./Footer"
 export default function Game()
 {
     const imagePaths = [
-        "./avocado/egg.png", "./images/banana.png", "./images/bread.png", "./images/brocolli.png", "./images/cheesecake.png", "./images/cherry.png",
+        "./images/avocado/.png", "./images/banana.png", "./images/bread.png", "./images/brocolli.png", "./images/cheesecake.png", "./images/cherry.png",
         "./images/coffee.png", "./images/croissant.png", "./images/cupcake.png", "./images/egg.png", "./images/fries.png", "./images/hamburger.png",
         "./images/ice-cream.png", "./images/nut.png", "./images/pizza.png", "./images/pumpkin.png", "./images/taco.png", "./images/watermelon.png"
     ]
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,12 +1,30 @@
+import avocado from "../images/avocado.png"
+import banana from "../images/banana.png"
+import bread from "../images/bread.png"
+import broccoli from "../images/broccoli.png"
+import cheesecake from "../images/cheesecake.png"
+import cherry from "../images/cherry.png"
+import coffee from "../images/coffee.png"
+import croissant from "../images/croissant.png"
+import cupcake from "../images/cupcake.png"
+import egg from "../images/egg.png"
+import fries from "../images/fries.png"
+import hamburger from "../images/hamburger.png"
+import iceCream from "../images/ice-cream.png"
+import nut from "../images/nut.png"
+import pizza from "../images/pizza.png"
+import pumpkin from "../images/pumpkin.png"
+import taco from "../images/taco.png"
+import watermelon from "../images/watermelon.png"
 
 export default function Tile(props)
 {
     const {tileObj} = props
 
     const imagePaths = [
-        "./images/avocado.png", "./images/banana.png", "./images/bread.png", "./images/broccoli.png", "./images/cheesecake.png", "./images/cherry.png",
-        "./images/coffee.png", "./images/croissant.png", "./images/cupcake.png", "./images/egg.png", "./images/fries.png", "./images/hamburger.png",
-        "./images/ice-cream.png", "./images/nut.png", "./images/pizza.png", "./images/pumpkin.png", "./images/taco.png", "./images/watermelon.png"
+        avocado, banana, bread, broccoli, cheesecake, cherry, 
+        coffee, croissant, cupcake, egg, fries, hamburger, 
+        iceCream, nut, pizza, pumpkin, taco, watermelon
     ]
 
     return (
